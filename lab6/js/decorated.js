6e5c90ff77d3db78669ecfd1db816c91254ac46e
aden <alimahatgreen@gmail.com>
2020-03-31 07:00:24 +0300

    lab6

---
+++ lab6/js/decorated.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  //setInterval(getBigger, 500);
+};
+var getBigger = function() {
+  var textArea = document.getElementById("textArea");
+  var current = textArea.style.fontSize;
+
+  if (current) {
+    current = current.substring(0, current.indexOf("p"));
+    current = parseInt(current) + 2;
+    textArea.style.fontSize = current + "pt";
+  } else {
+    textArea.style.fontSize = "24pt";
+  }
+};
+
+function getBold() {
+  var textArea = document.getElementById("textArea");
+  var checkBox = document.getElementById("checkBox");
+
+  if (checkBox.checked) {
+    textArea.style.fontWeight = "bold";
+    textArea.style.color = "green";
+    textArea.style.textDecoration = "underline";
+    document.body.style.backgroundImage =
+      "url('images/hundred-dollar-bill.jpg')";
+  } else {
+    textArea.style.fontWeight = "normal";
+    textArea.style.color = "black";
+    textArea.style.textDecoration = "none";
+    document.body.style.backgroundImage = null;
+  }
+}
